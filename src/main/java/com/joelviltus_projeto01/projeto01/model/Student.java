7e747c6503a6f43ce4e2dc3597d8ab5ac32bafed
Joel Viltus <joelviltus.edu@gmail.com>
2022-03-29 18:36:21 -0300

    First commit

---
+++ src/main/java/com/joelviltus_projeto01/projeto01/model/Student.java
@@ -0,0 +1,58 @@
+package com.joelviltus_projeto01.projeto01.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_student")
+public class Student {
+	
+	@Id
+	@GeneratedValue
+	private int idStudent;
+	
+	@Column(name = "name", nullable = false)
+	private String name;
+	
+	@Column(name = "email", nullable = false)
+	private String email;
+	
+	@Column(name = "age", nullable = false)
+	private int age;
+
+	public int getIdStudent() {
+		return idStudent;
+	}
+
+	public void setIdStudent(int idStudent) {
+		this.idStudent = idStudent;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
